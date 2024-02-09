18dc5c1195668eaf7a49590c6735b20d1c2047c9
Athulya-ts <113665715+Athulya-ts@users.noreply.github.com>
2024-02-09 12:15:36 +0530

    Delete BACKEND/mobile-recharge directory

--- BACKEND/mobile-recharge/src/main/java/com/recharge/mobilerecharge/enumerated/Role.java
+++
@@ -1,5 +0,0 @@
-package com.recharge.mobilerecharge.enumerated;
-
-public enum Role {
-    USER,ADMIN;
-}
--- BACKEND/mobile-recharge/src/main/java/com/recharge/mobilerecharge/repository/CustomerRepo.java
+++
@@ -1,8 +0,0 @@
-package com.recharge.mobilerecharge.repository;
-
-import com.recharge.mobilerecharge.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepo extends JpaRepository<Customer, Integer> {
-
-}
